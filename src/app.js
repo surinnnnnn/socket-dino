b9fcc25f4ee58c81b6501ef2668dffeb674c0500
surinnnnnn <didthfls2@naver.com>
2024-10-07 09:48:36 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import initSocket from "./init/socket.js";
 import { loadGameAssets } from "./init/assets.js";
 import { client, connectClient } from "./init/DBconnect.js";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
@@ -11,15 +13,20 @@ const server = createServer(app);
 
 const PORT = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // ㄴ> payload 를 자동으로 파싱해주는 미들웨어: 중첩객체(배열 또는 객체)의 처리를 제한한다.
 
 app.use("/assets", express.static("assets"));
 app.use(express.static("public"));
+
 initSocket(server);
+
 app.get("/", (req, res) => {
-     res.sendFile(__dirname + "/public/index.html");
+     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 server.listen(PORT, async () => {
      console.log(`Server is running on port ${PORT}`);
